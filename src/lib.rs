ba1bebba9e7d2edf84597dbeff581eb821404859
Mark Tyrkba <awfulmarco@gmail.com>
2025-08-13 00:29:34 +0300

    Add TODO(#6): Add a feature to strip `miette` dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 // TODO(#1): Implement tests
+// TODO(#6): Add a feature to strip `miette` dependency
 // TODO(#2): Implement F/D-extension: (single/double precision)
 // TODO(#4): Implement C-extension: (compressed instructions (16-bit))
 
